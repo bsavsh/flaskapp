370cab590c5743a1b73b3ee07e55accb4a888c9d
Bohdan Savshak <bohdan.savshak@sombrainc.com>
2020-04-25 18:14:41 +0300

    jinja language if condition

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    headline = "Hello, world!"
+    return render_template("index.html", headline=headline, new_year=new_year)
 
 @app.route("/<string:name>")
 def hello(name):
